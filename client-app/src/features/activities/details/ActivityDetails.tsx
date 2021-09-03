a283a8f30e7736ace588fb4dc7c916514ef21d0f
haluk.dogan@dmo.gov.tr <haluk.dogan@dmo.gov.tr>
2021-09-03 13:10:41 +0300

    first commit

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,36 @@
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Button, Card, Grid, Image } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { useStore } from '../../../app/stores/store';
+import ActivityDetailedHeader from './ActivityDetailedHeader'
+import ActivityDetailedInfo from './ActivityDetailedInfo'
+import ActivityDetailedChat from './ActivityDetailedChat'
+import ActivityDetailedSidebar from './ActivityDetailedSidebar'
+
+export default observer(function ActivityDetails(){
+
+    const {activityStore} = useStore();
+    const {selectedActivity:activity,loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams<{id: string}>();
+
+    useEffect(() => {
+      if(id) loadActivity(id);
+    }, [id, loadActivity]);
+
+    if (loadingInitial || !activity) return <LoadingComponent />;
+
+    return(
+        <Grid>
+          <Grid.Column width={10}>
+            <ActivityDetailedHeader/>
+            <ActivityDetailedInfo/>
+            <ActivityDetailedChat/>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <ActivityDetailedSidebar/>
+          </Grid.Column>
+        </Grid>
+    )
+}) 
